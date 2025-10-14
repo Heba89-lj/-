ffedc18edefa08f4eb307ed41bf329a0e3a5fb2c
Heba89-lj <h.jojo.loly.89@gmail.com>
2025-10-14 16:18:57 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,12 +2,12 @@ self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("qr-drive-cache").then(cache => {
       return cache.addAll([
-        "/qr-pwa/",
-        "/qr-pwa/index.html",
-        "/qr-pwa/main.html",
-        "/qr-pwa/manifest.json",
-        "/qr-pwa/icon-192.png",
-        "/qr-pwa/icon-512.png"
+        "/Heba89-lj/",
+        "/Heba89-lj/index.html",
+        "/Heba89-lj/main.html",
+        "/Heba89-lj/manifest.json",
+        "/Heba89-lj/icon-192.png",
+        "/Heba89-lj/icon-512.png"
       ]);
     })
   );
